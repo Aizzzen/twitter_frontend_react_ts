8678a53dbe62ff4da7b6297e5738b2431fc20c5d
Yunus <gadamurrr@gmail.com>
2023-03-07 00:01:17 +0300

    started making tweets column

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+        <CssBaseline/>
         <Router>
             <App/>
         </Router>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
-import {Container, Grid, IconButton, makeStyles, Paper, Typography} from "@material-ui/core";
+import {
+    Avatar,
+    Container,
+    createStyles,
+    Grid,
+    IconButton,
+    InputBase,
+    makeStyles,
+    Paper,
+    Typography,
+    withStyles
+} from "@material-ui/core";
 import TwitterIcon from '@material-ui/icons/Twitter';
 import SearchIcon from '@material-ui/icons/Search';
 import NotificationIcon from '@material-ui/icons/NotificationsNoneOutlined';
@@ -7,8 +18,8 @@ import MessageIcon from '@material-ui/icons/EmailOutlined';
 import BookmarkIcon from '@material-ui/icons/BookmarkBorderOutlined';
 import ListIcon from '@material-ui/icons/ListAltOutlined';
 import UserIcon from '@material-ui/icons/PermIdentityOutlined';
-import HomeIcon from '@material-ui/icons/HomeOutlined';
-import CreateIcon from '@material-ui/icons/Create';
+import { grey } from '@material-ui/core/colors';
+import CommentIcon from '@material-ui/icons/ChatBubbleOutline';
 
 const useStylesHomeStyle = makeStyles(() => ({
     wrapper: {
@@ -36,8 +47,38 @@ const useStylesHomeStyle = makeStyles(() => ({
     },
     navbarListItemIcon: {
         fontSize: 28
-    }
+    },
+    tweetsWrapper: {
+        borderRadius: 0,
+        height: '100%',
+        borderTop: 0,
+        borderBottom: 0,
+    },
+    tweetsHeader: {
+        borderRadius: 0,
+        borderTop: 0,
+        borderLeft: 0,
+        borderRight: 0,
+        padding: '10px 15px',
+        '& h6': {
+            fontWeight: 800,
+        }
+    },
+    tweetsUserName: {
+        color: grey[500]
+    },
 }))
+
+const SearchTextField = withStyles(() =>
+    createStyles({
+        input: {
+            borderRadius: 30,
+            backgroundColor: '#E6ECF0',
+            height: 45,
+            padding: 0
+        }
+    })
+)(InputBase)
 
 export const Home = () => {
     const classes = useStylesHomeStyle();
@@ -91,10 +132,42 @@ export const Home = () => {
                     </ul>
                 </Grid>
                 <Grid item xs={6}>
-                    <Paper>xs</Paper>
+                    <Paper className={classes.tweetsWrapper} style={{ height: '100%' }} variant='outlined'>
+                        <Paper className={classes.tweetsHeader} variant='outlined'>
+                            <Typography variant='h6'>Главная</Typography>
+                        </Paper>
+                        <Paper className={classes.tweetsHeader} variant='outlined'>
+                            <Grid container spacing={3}>
+                                <Grid item xs={1}>
+                                    <Avatar
+                                        alt='User Avatar'
+                                        src='https://images.unsplash.com/photo-1541562232579-512a21360020?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+                                    />
+                                </Grid>
+                                <Grid item xs={11}>
+                                    <Typography>
+                                        <b>gadamurrr</b> <span className={classes.tweetsUserName}>@gadamurrr</span>
+                                    </Typography>
+                                    <Typography variant='body1' gutterBottom>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam, culpa cumque dolores ea exercitationem facere id iusto molestiae molestias mollitia nemo perferendis possimus reiciendis repellat ullam voluptate! Nostrum, voluptatibus.
+                                    </Typography>
+
+                                    <div>
+                                        <IconButton>
+                                            <CommentIcon style={{fontSize: 16}} />
+                                        </IconButton>
+                                        <span>1</span>
+                                    </div>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Paper>
                 </Grid>
                 <Grid item xs={3}>
-                    <Paper>xs</Paper>
+                    <SearchTextField
+                        placeholder='Поиск по Твиттеру'
+                        fullWidth
+                    />
                 </Grid>
             </Grid>
         </Container>
