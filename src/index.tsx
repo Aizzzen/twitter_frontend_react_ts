42dfc2caa5eaff5571dbc91cee8e39c84238df6d
Yunus <gadamurrr@gmail.com>
2023-03-06 12:37:40 +0300

    added mui version 4

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import theme from "./theme";
+import { ThemeProvider } from '@material-ui/core';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// т.к. MUI это css in js все стили будут прикручиваться через функции
+// для того чтобы это работало для всего приложения оборачиваем его в ThemeProvider
+
+// MUI 5 версии не работал на фукнции makeStyles() поэтому перешел на 4 версию
